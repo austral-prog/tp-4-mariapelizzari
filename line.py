7fe9420d020de5fe7030ee0e698182d0aec7a4e7
mariapelizzari <mpelizzari@mail.austral.edu.ar>
2025-04-11 12:57:29 -0300

    Update line.py

--- line.py
+++ line.py
@@ -3,24 +3,24 @@ def line():
     coeficiente_B = float(input("Ingrese el coeficiente B: "))
     coeficiente_x1 = float(input("Ingrese el coeficiente X1: "))
     coeficiente_x2 = float(input("Ingrese el coeficiente X2: "))
-
+    coeficiente_y1= (coeficiente_A * coeficiente_x1 + coeficiente_B)
+    coeficiente_y2= (coeficiente_A * coeficiente_x2 + coeficiente_B)
     print(f"El coeficiente A de su ecuación de la recta es: {coeficiente_A}")
     print(f"El coeficiente B de su ecuación de la recta es: {coeficiente_B}")
     print(f"El coeficiente X1 de su ecuación de la recta es: {coeficiente_x1}")
     print(f"El coeficiente X2 de su ecuación de la recta es: {coeficiente_x2}")
+   
     print(f"\nPara la siguiente ecuación:")
     print(f"\tY = {coeficiente_A}X + {coeficiente_B}")
+   
     print(f"\nDados los siguientes puntos:")
-    Y1=coeficiente_A*coeficiente_x1+coeficiente_B
-    print(f"\t{Y1}")
-    
-    P1= (50.0, 110.99999999999999)
-    P2= (-32.9, -79.66999999999999)
-    x1= 50.0
-    y1= 110.99999999999999
-    x2= -32.9
-    y2= -79.66999999999999
+    p = [coeficiente_x1, coeficiente_y1]
+    q = [coeficiente_x2, coeficiente_y2]
 
-	print(f"\nDados los siguientes puntos:\n \tP1 ({x1},{y1})\n\tP2 ({x2},{y2})")
-    distancia=float(((y2-y1)**2 + (x2-x1)**2)**0.5)
+    P1_str = f"P1 ({coeficiente_x1}, {coeficiente_y1})"
+    P2_str = f"P2 ({coeficiente_x2}, {coeficiente_y2})"
+    print(f"\t{P1_str}")
+    print(f"\t{P2_str}")
+    distancia = float(((coeficiente_y2-coeficiente_y1)**2+ (coeficiente_x2-coeficiente_x1)**2)**0.5)
     print(f"\nLa distancia entre ellos es: {distancia}")
+    
